d08fa3b5f3d92c8f10ca6850878019a6490af225
BTT-22 <33435030+BTT-22@users.noreply.github.com>
2018-12-19 12:18:40 +0100

    Update
    Found a way to make the app update its RDF stores. This still requires manually replacing the prefixes, but I'll make a function to do that automatically (eventually).

--- index.js
+++ index.js
@@ -40,19 +40,21 @@ app.get('/rdf2', (req, res) => {
 	 	WHERE {
 			?s foaf:name ?name .
 	 	}
-	8) run .\fuseki-server --file staff.rdf /Test
-	9) navigeer naar 
+	8) run ".\fuseki-server --mem --update /Test" in de uitgezipte map van fuseki
+	9) NIEUW: upload de file test.rdf
+		(om de een of andere reden werkt updaten niet als ge op een file werkt)
+	10) navigeer naar 
 	http://localhost:3030/Test/query?query=SELECT%20DISTINCT%20%3Fclass%20WHERE%20%7B%3Fs%20a%20%3Fclass%20.%7D
 		(kan zijn dat het andere poort is, kijk naar output van fuseki-server)
 		Dit is dezelfde query als 7a, maar over HTTP
-	10) open nieuwe terminal
-	11) run npm i
-	12) wacht tot alles geinstalleerd is
-	13) run node.js
-	14) ga naar /rdf2
-	15) meld resultaten
-	16) ???
-	17) Profit!
+	11) open nieuwe terminal
+	12) run "npm i"
+	13) wacht tot alles geinstalleerd is
+	14) run "node index.js"
+	15) ga naar /rdf2
+	16) meld resultaten
+	17) ???
+	18) Profit!
 
 	Als er iets fout gaat:
 		opzoeken en bidden
@@ -68,6 +70,51 @@ app.get('/rdf2', (req, res) => {
 	query = query.replace('foaf:name', '<http://xmlns.com/foaf/0.1/name>');
 	console.log(query)
 
+	endpoint.selectQuery(query).then(function(resp){
+		return resp.text();
+	}).then(function(body) {
+		//parse body
+		var result = JSON.parse(body);
+		//output
+		var output = JSON.stringify(result, null, ' ');
+		console.log(output);
+		res.send(output);
+	}).catch(function (err){
+		console.error(err);
+	})
+});
+
+app.get('/rdf2/update', (req, res) => {
+	var fetch = require('isomorphic-fetch')
+	var SparqlHttp = require('sparql-http-client')
+
+	SparqlHttp.fetch = fetch;
+
+	var endpoint = new SparqlHttp({endpointUrl: 'http://localhost:3030/Test/update'});
+	var query = `INSERT {?p a ulbterm:Professor} WHERE {?p foaf:name "François Picalausa".}`;
+	query = query.replace('foaf:name', '<http://xmlns.com/foaf/0.1/name>');	
+	query = query.replace('ulbterm:Professor', '<http://code.ulb.ac.be/example/terms/Professor>');
+	console.log(query)
+
+	endpoint.selectQuery(query).then(function(resp){
+		return resp.text();
+	}).then(function(body) {
+		//parse body
+		var result = JSON.parse(body);
+		//output
+		var output = JSON.stringify(result, null, ' ');
+		console.log(output);
+	}).catch(function (err){
+		console.error(err);
+	})
+
+	//now check if it actually went through!
+	endpoint = new SparqlHttp({endpointUrl: 'http://localhost:3030/Test/query'});
+	query = `SELECT DISTINCT ?name {?p foaf:name ?name. ?p a ulbterm:Professor .}`;
+	query = query.replace('foaf:name', '<http://xmlns.com/foaf/0.1/name>');	
+	query = query.replace('ulbterm:Professor', '<http://code.ulb.ac.be/example/terms/Professor>');
+	console.log(query);
+	
 	endpoint.selectQuery(query).then(function(resp){
 		return resp.text();
 	}).then(function(body) {
