690d02603e4f0da1f8762202c18da9c4e1698f9e
BTT-22 <33435030+BTT-22@users.noreply.github.com>
2018-12-21 20:47:10 +0100

    Code refactoring
    Cleaned up the code a little; handling prefixes is now done fairly automatically.

--- index.js
+++ index.js
@@ -7,18 +7,44 @@ app.use(helmet())
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
+//left in as an example of how to accept params
 app.get('/user/:name', (req, res) => {
 	res.send('Hello ' + req.params.name + '!');
 });
 
-function test(){
-	return 'lol';
+function handleRDFPrefix(query, prefixShort, prefixLong){
+	let i = query.indexOf(prefixShort);
+	if(i === -1)
+		return query;
+	//else: 
+	//we know the short prefix is in the query
+	//so we'll need to replace it
+	let tmp = query.substr(i, query.length);	
+
+	//tmp is now prefixShort:xxxx
+	tmp = tmp.substr(0,tmp.indexOf(" "));
+
+	//tmp2 is now xxxx
+	let tmp2 = tmp.substr(tmp.indexOf(":") + 1, tmp.length);
+	//make tmp2 into the full, non-shortened URI
+	tmp2 = "<" + prefixLong + tmp2 + ">";
+
+	return query.replace(tmp, tmp2);
 }
 
-app.get('/asynctest', async (req, res) => {
-	let t = await test();
-	res.send(t);
-})
+function handleAllPrefixesKnown(query){
+	query = handleRDFPrefix(query, 'linkrec', 'http://linkrec.be/terms');
+	query = handleRDFPrefix(query, 'rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
+	query = handleRDFPrefix(query, 'foaf', 'http://xmlns.com/foaf/0.1/');
+	console.log(query);
+	return query;
+}
+
+function prettifyJSONResponse(obj){
+	let tmp = obj.results.bindings;
+	tmp = JSON.stringify(tmp, null, ' ');
+	return tmp;
+}
 
 app.get('/rdf2', (req, res) => {
 	var fetch = require('isomorphic-fetch')
@@ -27,19 +53,20 @@ app.get('/rdf2', (req, res) => {
 	SparqlHttp.fetch = fetch
 
 	var endpoint = new SparqlHttp({endpointUrl: 'http://localhost:3030/Test/query'});
-	//var query = 'SELECT DISTINCT ?class WHERE {?s a ?class .}';
-	var query = `SELECT ?name WHERE {?s foaf:name ?name .}`;
-	query = query.replace('foaf:name', '<http://xmlns.com/foaf/0.1/name>');
-	console.log(query)
+	var query = `SELECT DISTINCT ?name ?degreename WHERE 
+	{
+		?s foaf:name ?name .
+		?s linkrec:degree ?degree .
+		?degree rdf:value ?degreename .
+	}`;
+	query = handleAllPrefixesKnown(query);
 
 	endpoint.selectQuery(query).then(function(resp){
 		return resp.text();
 	}).then(function(body) {
 		//parse body
 		var result = JSON.parse(body);
-		//output
-		var output = JSON.stringify(result, null, ' ');
-		console.log(output);
+		let output = prettifyJSONResponse(result);
 		res.send(output);
 	}).catch(function (err){
 		console.error(err);
@@ -48,7 +75,8 @@ app.get('/rdf2', (req, res) => {
 
 app.get('/rdf2/update', (req, res) => {
 
-	res.send("Deze endpoint werkt niet op het moment.");
+	res.send("Deze endpoint werkt niet op het moment; ze is er alleen ingelaten om te laten zien hoe te updaten.");
+	return;
 
 	var fetch = require('isomorphic-fetch')
 	var SparqlHttp = require('sparql-http-client')
@@ -57,9 +85,7 @@ app.get('/rdf2/update', (req, res) => {
 
 	var endpoint = new SparqlHttp({endpointUrl: 'http://localhost:3030/Test/update'});
 	var query = `INSERT {?p a ulbterm:Professor} WHERE {?p foaf:name "François Picalausa".}`;
-	query = query.replace('foaf:name', '<http://xmlns.com/foaf/0.1/name>');	
-	query = query.replace('ulbterm:Professor', '<http://code.ulb.ac.be/example/terms/Professor>');
-	console.log(query)
+	query = handleAllPrefixesKnown(query);
 
 	endpoint.selectQuery(query).then(function(resp){
 		return resp.text();
