3004560e8430c3cc4e351b6b7420a52d757a972b
BTT-22 <33435030+BTT-22@users.noreply.github.com>
2018-12-23 14:52:33 +0100

    More intricate JSON prettification

--- users.js
+++ users.js
@@ -4,15 +4,61 @@ router.use(express.json());
 router.use(express.urlencoded());
 var QueryBuilder = require('./querybuilder')
 
-router.get('/test', function (req, res) {
-	res.send('Users home page.');
-})
+function prettifyJSONAlt(obj){
+	var newobj = [];
+	for(var singleQueryObj in obj){
+		let singleQuery = obj[singleQueryObj];
+		let newObjItem = {};
+		for(var singleQueryItem in singleQuery){
+			let sqItem = singleQuery[singleQueryItem];
+			//sqItem is now something like {type: 'literal', value:'xxxxx'}
+			let tmp = sqItem['value'];
+			newObjItem[singleQueryItem] = tmp;
+		}
+		newobj.push(newObjItem);
+	}
+	return newobj;
+}
 
 function prettifyJSONResponse(obj){
 	let tmp = obj.results.bindings;
+	tmp = prettifyJSONAlt(tmp);
 	tmp = JSON.stringify(tmp, null, ' ');
+	//console.log(tmp);
 	return tmp;
 }
+
+router.get('/', (req, res) => {
+	var fetch = require('isomorphic-fetch')
+	var SparqlHttp = require('sparql-http-client')
+
+	SparqlHttp.fetch = fetch;
+
+	var endpoint = new SparqlHttp({endpointUrl: 'http://localhost:3030/Test/query'});
+	var query = `SELECT DISTINCT ?name ?degreename ?email ?bio WHERE 
+	{
+		?p foaf:name ?name .
+		?p linkrec:degree ?degree .
+		?degree rdf:value ?degreename .
+		?p vcard:email ?email .
+		?p linkrec:BIO ?bio .
+	}`;
+	qb = new QueryBuilder(query);
+	// PLEASE NOTE: execute bindParam BEFORE handling prefixes, otherwise it might not properly handle the param's type!!
+	qb.handleAllPrefixesKnown();
+
+	endpoint.selectQuery(qb.result()).then(function(resp){
+		return resp.text();
+	}).then(function(body) {
+		//parse body
+		var result = JSON.parse(body);
+		let output = prettifyJSONResponse(result);
+		res.send(output);
+	}).catch(function (err){
+		console.error(err);
+		res.send("Error!");
+	})
+});
 
 router.get('/:id', (req, res) => {
 	var fetch = require('isomorphic-fetch')
